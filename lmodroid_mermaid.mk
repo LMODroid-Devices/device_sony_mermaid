d3aa8abecff17d3d80f6fa949c6185980a57069e
Erfan Abdi <erfangplus@gmail.com>
2022-08-04 02:19:57 +0430

    Adapt to LMODroid by device-add script
    Change-Id: Ic9481125ada03a20c3dbe7c554d49aa21d57a0c7

--- lmodroid_mermaid.mk
+++ lmodroid_mermaid.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/sony/mermaid/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_mermaid
+PRODUCT_NAME := lmodroid_mermaid
 PRODUCT_DEVICE := mermaid
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := Xperia 10 Plus
